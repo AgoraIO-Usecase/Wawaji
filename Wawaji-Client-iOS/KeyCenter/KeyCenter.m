e95825a857c3c6e4608238d11d5b8039b478f82a
苏乐雨 <suleyu@agora.io>
2017-11-22 07:26:53 -0600

    iOS with ObjectiveC: another solution to switch camera (#7)

--- Wawaji-Client-iOS/KeyCenter/KeyCenter.m
+++ Wawaji-Client-iOS/KeyCenter/KeyCenter.m
@@ -1,11 +1,11 @@
 //
-//  KeyCenter.h
+//  KeyCenter.mm
 //  Wawaji-Client-iOS
 //
 //  Created by suleyu on 2017/11/22.
 //  Copyright © 2017 Agora. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import "KeyCenter.h"
 
-extern NSString *const kAgoraAppID;
+NSString * const kAgoraAppID = <#Your App ID#>;
