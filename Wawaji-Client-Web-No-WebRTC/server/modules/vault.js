dfcea6113e42f993e670170e5652d90101545322
张乾泽 <qianze.zhang@hotmail.com>
2017-11-24 14:55:08 +0800

    remove valut

--- Wawaji-Client-Web-No-WebRTC/server/modules/vault.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    // appid: "YOUR APP ID",
-    // appcert: "YOUR CERTIFICATE"
-    appid: "324f0da1e2284832a44fee5fcbec44c1",
-    appcert: "164aa13965394ffbb5ebeb43c4c7ed5c"
-}
