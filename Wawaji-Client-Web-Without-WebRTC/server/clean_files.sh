543e6860d9872357c31ef853277e38232872ee0c
Hai Guo <deliberatekids@gmail.com>
2017-12-01 15:54:27 +0800

    init rtc-simple-control

--- Wawaji-Client-Web-Without-WebRTC/server/clean_files.sh
+++
@@ -1,6 +0,0 @@
-#delete files earlier than 1 mins
-find `pwd`/public/**/-*.jpg -type f -mmin +1 -delete
-find `pwd`/public/**/*.jpg -type f -mmin +1 -delete
-
-now=$(date +"%T")
-echo "Current time : $now" > `pwd`/clean.log
