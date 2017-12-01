eae60178ab3e227334b51ea47b9d8154e5bf8899
Hai Guo <deliberatekids@gmail.com>
2017-12-01 13:44:36 +0800

    move it to solution branch

--- Wawaji-Client-Android/README.en.md
+++
@@ -1,4 +0,0 @@
-# Wawaji client for Android
-
-*其他语言版本： [简体中文](README.md)*
-
--- Wawaji-Client-Web-H5/client/src/assets/js/vault_template.js
+++
@@ -1,7 +0,0 @@
-var Vault = (function(){
-    return {
-        // appid : "YOUR APP ID",
-        // appcert : "YOUR CERTIFICATE",
-        // cc_server : "YOUR CENTER CONTROL MACHINE NAME"
-    }
-}());
